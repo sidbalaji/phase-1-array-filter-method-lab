1d458da15726460b63d2afee5975582ca04c8587
Siddarth Balaji <sid.balaji@berkeley.edu>
2023-01-13 15:13:23 -0500

    completed lab with all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+
+function findMatching(arr, name){
+    return arr.filter(
+        (driver)=> driver.toLowerCase() === name.toLowerCase()
+                          )
+}
+
+function fuzzyMatch(arr, letters){
+    return arr.filter(
+        (driver) => driver.startsWith(letters)
+    )
+}
+function matchName(arr, letters){
+    return arr.filter(
+        (drivObj)=> drivObj.name === letters
+                      )
+}
